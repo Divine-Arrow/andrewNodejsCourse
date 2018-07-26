9954148115cefc94a0235c53749e940d74718284
Bhupender Singh <bhupenders225@gmail.com>
2018-07-26 23:21:21 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+var fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// middlewares
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `saving log at "${now}" for the route of : "${req.url}".. \n`;
+    fs.appendFile('server.log', log, (err)=> {
+        if(err) {
+            console.log('something went wrong..!');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next)=> {
+    res.render('maintenance');
+});
+
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static('./public'));
+ 
+// hbs helpers
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// Register a handler
+app.get('/', (req, res) => {
+    res.render('home', {
+        welcomeMessage: 'Welcome to the HOME PAGE'
+    });
+});
+
+// about page
+app.get('/about', (req, res) => {
+    res.render('about', {
+        data: 'about page',
+        time: new Date().toLocaleTimeString()
+    });
+})
+
+
+// Listen for requests
+app.listen(3000, () => {
+    console.log('App is started.!');
+});
